b43a76314165be30aeca98b75c7729e6c2319fba
marcosvb1 <marcosvb1@gmail.com>
2025-09-27 21:49:02 -0300

    fix: Corrigir build e republicar versão 0.1.18
    - Resolvido problema de compilação TypeScript
    - Pasta dist agora é criada corretamente
    - Novo ícone incluído na distribuição
    - Correções do recurso leads mantidas
    - Versão 0.1.18 com build funcional

--- credentials/KommoLongLivedApi.credentials.d.ts
+++
@@ -1,8 +0,0 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
-export declare class KommoLongLivedApi implements ICredentialType {
-    name: string;
-    displayName: string;
-    documentationUrl: string;
-    icon: "file:kommo_logo.svg";
-    properties: INodeProperties[];
-}
